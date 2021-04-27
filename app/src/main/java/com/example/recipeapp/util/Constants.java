c4893fea0a6cd935436574bf244e2a14eb6f8f50
CyloTM <samuelchyke@hotmail.com>
2021-04-27 14:32:33 +0000

    Constants Setup

---
+++ app/src/main/java/com/example/recipeapp/util/Constants.java
@@ -0,0 +1,26 @@
+package com.example.recipeapp.util;
+
+public class Constants {
+
+    public static final String BASE_URL = "https://recipesapi.herokuapp.com";
+
+    public static final String APP_KEY = "";
+
+    public static final int CONNECTION_TIMEOUT = 10; // seconds
+    public static final int READ_TIMEOUT = 10; // seconds
+    public static final int WRITE_TIMEOUT = 10; // seconds
+
+    public static final String[] DEFAULT_SEARCH_CATEGORIES =   {"Barbecue", "Breakfast","Chicken","Beef","Brunch","Dinner","Wine","Italian" };
+
+    public static final String[] DEFAULT_SEARCH_IMAGES =
+            {
+            "barbecue",
+            "breakfast",
+            "chicken",
+            "beef",
+            "brunch",
+            "dinner",
+            "wine",
+            "italian"
+            };
+}
