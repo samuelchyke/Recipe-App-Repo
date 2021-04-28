43a15ce246d7b76b929dee8825c1f31f68915a50
CyloTM <samuelchyke@hotmail.com>
2021-04-28 14:17:15 +0000

    Retrofit Interface Methods

--- app/src/main/java/com/example/recipeapp/requests/RecipeApi.java
+++ app/src/main/java/com/example/recipeapp/requests/RecipeApi.java
@@ -1,5 +1,28 @@
 package com.example.recipeapp.requests;
 
+import com.example.recipeapp.requests.response.RecipeSearchResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface RecipeApi {
+
+    // Search
+    @GET("api/search")
+    Call<RecipeSearchResponse> searchRecipe(
+            @Query("key") String key,
+            @Query("q") String query,
+            @Query("page") String page
+    );
+
+    // Get
+    @GET("api/get")
+    Call<RecipeSearchResponse> getRecipe(
+            @Query("key") String key,
+            @Query("rId") String recipe_id
+    );
+
+
 
 }
