f643376e524c0d1b9ed415e4ecb76d0e289f8f70
CyloTM <samuelchyke@hotmail.com>
2021-04-29 15:13:34 +0000

    RecyclerView Adapter

---
+++ app/src/main/java/com/example/recipeapp/adapters/RecipeRecyclerAdapter.java
@@ -0,0 +1,51 @@
+package com.example.recipeapp.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.recipeapp.R;
+import com.example.recipeapp.models.Recipe;
+
+import java.util.List;
+
+public class RecipeRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private List<Recipe> mRecipes;
+    private OnRecipeListener mOnRecipeListener;
+
+    public RecipeRecyclerAdapter(List<Recipe> mRecipes, OnRecipeListener mOnRecipeListener) {
+        this.mRecipes = mRecipes;
+        this.mOnRecipeListener = mOnRecipeListener;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_recipe_list_item, viewGroup, false);
+        return new RecipeViewHolder(view, mOnRecipeListener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+
+        ((RecipeViewHolder)viewHolder).title.setText(mRecipes.get(i).getTitle());
+        ((RecipeViewHolder)viewHolder).publisher.setText(mRecipes.get(i).getPublisher());
+        ((RecipeViewHolder)viewHolder).socialScore.setText(String.valueOf(Math.round(mRecipes.get(i).getSocial_rank())));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mRecipes.size();
+    }
+
+    public void setRecipes(List<Recipe> recipes){
+        mRecipes = recipes;
+        notifyDataSetChanged();
+    }
+
+}
+
